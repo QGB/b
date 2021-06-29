4c0ca9b7db70adc7d3f25b93641f4f3dec4f7ed5
qgb <qgbcs1@gmail.com>
2021-06-29 07:26:29 +0000

    init

---
+++ .bash_history
@@ -0,0 +1,6 @@
+git clone https://github.com/QGB/qpsu qgb;git clone https://github.com/QGB/miniconda3;./miniconda3/bin/ipython
+cd ~/app
+ll
+git init
+cd ..
+history -w
---
+++ test/ipy/pip install youtube_dl.py
@@ -0,0 +1,29 @@
+#coding=utf-8
+import sys;'qgb.U' in sys.modules or sys.path.append('/home/leanengine/');from qgb import *
+
+#get_ipython().run_line_magic('pip', 'install youtube_dl')              # 1 
+#get_ipython().run_cell_magic('', '', 'U.cdqp()\n\n')                   # 2 
+#U.cdqp()
+get_ipython().system('git pull origin master')
+U.r(py,U,T,N,F,N.HTTP,N.HTML,)                                          # 3 
+#from qgb import *                                                      # 4 
+_5=N.rpcServer(globals=globals(),locals=locals(),)                      # 5 
+#U.cdqp()
+get_ipython().system('git pull origin master')
+U.r(py,U,T,N,F,N.HTTP,N.HTML,)                                          # 6 
+#_7=ipy.save(_i1)                                                       # 7 
+#_8=get_ipython().run_line_magic('pwd', '')                             # 8 
+_9=U.cdb()                                                              # 9 
+#get_ipython().system('git init')                                       # 10 
+#du=get_ipython().getoutput('du -m|sort -n')                            # 11 
+#get_ipython().run_line_magic('cd', '.cache/')                          # 12 
+#duc=get_ipython().getoutput('du -m|sort -n')                           # 13 
+dun=[i for i in du if not U.one_in(['/miniconda3/','/v2ray-linux-64/'],i)  ]  # 14 
+_15=U.get('t_form')                                                     # 15 
+_16=U.get('t_form')['t']                                                # 16 
+_17=F.write(F.get_home()+'.gitignore',U.get('t_form')['t'] )            # 17 
+#get_ipython().run_line_magic('ll', '')                                 # 18 
+#get_ipython().run_line_magic('cd', '~')                                # 19 
+#get_ipython().run_line_magic('ll', '')                                 # 20 
+#get_ipython().run_line_magic('ll', '-alh')                             # 21 
+#ipy.save()                                                             # 22 
